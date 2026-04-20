e0e54f4fddfb24de12f50fe985bb8f14e25134c4
skanpl <savankan089@gmail.com>
2026-04-20 03:38:55 +0200

    destruct>induction here but client action is still a pain

--- theories/Testing_Pre_orders/Must/CtxCompose.v
+++ theories/Testing_Pre_orders/Must/CtxCompose.v
@@ -10,6 +10,61 @@ Notation "p << q" := (@ctx_pre _ _ _ _ _ _ proc _ _ _ _ _ _ _ p q) (at level 40)
 Notation tauact q := (t • q).
 
 
+(*
+Proposition ctx_compose_tau: forall p q, p << q -> 
+  (g (gpr_tau p)) << (g (gpr_tau q)).
+Proof.
+intros p q Hmust.  
+unfold "<<" in *.
+intros e Hmp.
+generalize dependent q. 
+destruct Hmp.  
+- eauto with mdb. 
+- (*intros q Htest.*) destruct ex as [r trans]. 
+  inversion trans;subst; intros.
+  + inversion l; subst. intros.
+    eapply m_step; eauto with mdb.
+   *eexists. do 2 constructor.
+   *intros p' Htau. inversion Htau. subst.
+    eauto with mdb.
+  * intros e' He.
+    clear com. 
+    specialize (et _ He). 
+    specialize (pt _ l).
+    (*             
+            τ.a2  MP  e'
+             ↓
+             a2
+
+     ==>   
+             τ.a2  MP  e'
+             ↓    MP
+             a2
+ 
+   thus:       ...
+            —————————— 
+             a2 MP e' 
+           ————————————Hmust
+            q MP e'
+
+
+*)   
+ 
+    assert (q must_pass e'). admit.
+    admit.
+ 
+  * admit. 
+
++ clear com.
+  specialize (et _ l).
+  tau.q MP
+p MP e          tau.p MP b2
+*)
+
+
+
+
+
 
 
 Proposition ctx_compose_tau: forall p q, p << q -> 
@@ -18,78 +73,71 @@ Proof.
 intros p q Hmust.  
 unfold "<<" in *.
 intros e Hmp.
-(*generalize dependent q.*) 
-induction Hmp.  
+destruct Hmp.  
 - eauto with mdb. 
 - (*intros q Htest.*) destruct ex as [r trans]. 
   inversion trans;subst.
-  + eauto with mdb. 
-  + eapply m_step; eauto with mdb.
-    * eexists. do 2 constructor.
-    * intros p' Htau.
-      clear H1 com pt H.
-      specialize (et _ l). specialize (H0 _ l).
-      inversion Htau. subst.    
-      admit. 
+  inversion l; subst.
+  eapply m_step; eauto with mdb.
+
+  * eexists. do 2 constructor.
+  * intros p' Htau. inversion Htau. subst.
+    eauto with mdb.
+  * intros e' He.
+    clear com. 
+    specialize (et _ He). 
+    specialize (pt _ l).
+    (* 
+         
+
+find something
+==>   
+            τ.q   MP  e
+                      ↓
+                      e'  
+
+  ==>
+            τ.q   MP  e
+               MP     ↓
+                      e'  
+
+*)   
+     admit. 
+  * intros p' e' μ1 μ2 pi Htau He.
+    inversion Htau.
+  * 
+    specialize (et _ l).
+   (* 
+        τ.p  MP  b2
+         ↓    (MP)
+         p
+
+Hmust
+==>
+
+  q MP b2
 
 
-    * intros p' e' μ1 μ2. 
-      intros Hpi Hmu1 Hmu2.
-      clear pt H.
-      specialize (et _ l). specialize (H0 _ l).
-      admit.
+==> ???
 
+    *)
+   (*
+   (*======  bidouillage=========*)
+   eapply m_step; eauto with mdb. 
+   eexists. do 2 constructor.
+   intros. inversion H. subst.
+   clear com.
+   specialize (pt p).
+   eapply Hmust.
+   eapply pt.
+   constructor.
 
-      
-  + eapply m_step; eauto with mdb.
-    * eexists. do 2 constructor. 
-    * clear pt H et H0.
-      intros p' Htau.
-      inversion Htau. subst.
-      admit.
-    * intros. 
-      clear pt H et H0.
-      admit.
+   intros e' He.
+   clear com.
+   Focus 2. intros. inversion H0. 
+   (*==========================*)
+  *)
+   admit.
+  * inversion l1.  
+
 Admitted.
-
-
-
-
-
-
-
-(*
-Require Import Coq.Program.Equality.
-
-Proposition ctx_compose_tau2: forall p q, p << q -> 
-  (g (gpr_tau p)) << (g (gpr_tau q)).
-Proof.
-intros p q Hmust.  
-unfold "<<" in *.
-intros e Hmp.
-(*generalize dependent q.*) 
-dependent induction Hmp.  
-- eauto with mdb. 
-- destruct ex as [r trans]. 
-  inversion trans;subst.
-  + clear H1 com H0 et H.
-     
-  + clear H1 com H pt.  
-    eapply m_step; eauto with mdb.
-    * eexists. do 2 constructor.
-    * intros p' Htau.
-      specialize (et _ l). specialize (H0 _ l).    
-      admit. 
-    * admit.
-
-  + eapply m_step; eauto with mdb.
-    * eexists. do 2 constructor. 
-    * admit.
-    * intros. 
-      clear pt H et H0.
-      admit.
-Admitted.
-    
-*)        
-    
-
