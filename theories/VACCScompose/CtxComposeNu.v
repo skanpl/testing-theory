d2f1e36f03a1dbd50467540c545b1fd348dd39bb
skanpl <savankan089@gmail.com>
2026-05-18 15:09:08 +0200

    new compose finished (or maybe not)

--- theories/VACCScompose/CtxComposeNu.v
+++ theories/VACCScompose/CtxComposeNu.v
@@ -60,22 +60,43 @@ Lemma dual_shift: forall (mu1 mu2:ExtAct TypeOfActions),
   parallel_inter (ash mu1) (ash mu2).
 
 Proof. 
-Admitted.
-
-
+intros; cbv in H.
+destruct mu1,mu2,a0; try (exfalso; apply H); subst; cbn; auto.
+Qed.
 
 Lemma dual_shift_inv: forall mu1 mu2, parallel_inter mu1 (ash mu2) ->
   exists mu0, mu1 = ash mu0 /\ parallel_inter mu0 mu2.
 
-Proof. 
+Proof.
+intros. assert (parallel_inter mu1 (ash mu2)); auto; cbn in H.
+unfold ext_act_match in H.
+destruct mu1,mu2.
+-  assert (exists a', ash (ActIn a0) = ActIn a' ).
+   unfold ash; destruct a0; eauto.
+  destruct H1. rewrite H1 in H. exfalso; auto.  
+- assert (exists a', ash (ActOut a0) = ActOut a' ).
+   unfold ash; destruct a0; eauto.
+   destruct H1. rewrite H1 in H; simpl in H.
+   subst.
+   rewrite H1 in H0.
+   exists (ActIn a0).
+   admit.
+- admit.
+- assert (exists a', ash (ActOut a0) = ActOut a' ). 
+   unfold ash; destruct a0; eauto.
+   destruct H1; rewrite H1 in H; exfalso; auto.
 Admitted.
 (*------------- other stuff --------------------------*)
 	
 Lemma ash_inj: forall (mu1 mu2:ExtAct TypeOfActions), 
   ash mu1 = ash mu2 -> mu1=mu2.
 
-Proof. 
-Admitted.
+Proof.
+intros; unfold ash in H.
+destruct mu1,mu2,a,a0; inversion H; subst.
+cbv in H1; destruct c,c0; inversion H ; auto.
+unfold VarC_add in H1; destruct c,c0; inversion H; auto.
+Qed.
 
 
 
@@ -126,9 +147,6 @@ Qed.
 
 
 
-(*the only thing left to show in the following lemma is to show that
- a synchro between p and (shift e) preserve the must predicate.
-*)
 Lemma mp_fromnu: forall (p e: proc),
   ν p must_pass e -> p must_pass shift e.
 Proof.
@@ -143,83 +161,17 @@ dependent induction H; eauto with mdb.
     + inversion l1; subst. set (lem:= lts_shift_mu _ _ _ l2);
       eexists; eapply ParSync; try eapply dual_shift; eauto.
   * intros p' Hp.
-    (*je m'étais fait avoir par le parsing de Rocq,je pensais que H se lisait:
-       H : forall p' : proc, (ν p) ⟶ p' -> 
-          (forall p : proc, p' = ν p) ->  p must_pass shift e   
-           ^^^^^^^^^^^^^^^^^^^^^^^^^^
-    mais enfaite ca se lit:
-       H : forall p' : proc, (ν p) ⟶ p' -> 
-          (forall p : proc, p' = ν p -> p must_pass shift e )
-          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-      du coup je pensait etre coincé mais enfaite non, my bad *)
-
     eapply H; try constructor; eauto.
   * intros e' Hse.
     set (lem:= lts_shift_inv_tau _ _ Hse).
     destruct lem as [e1 [He Heeq]]; subst; eauto.
-
-  * intros p' E ? ? Hpi Hp Hse. (*here, the goal is to show that a synchro*)
-    cbv in Hpi.                 (*between p and (shift e) preserves must*)
-    destruct μ1,μ2,a0; subst; try (exfalso; apply Hpi).
-    + destruct c.  
-      
-      Print ChannelData. (*why is a channel either a cstC or bvarC  ??? *)
-      (*wouldn't it be more reasonable to have 
-        the type channel to be a 1-constructor ADT:
-        Inductive channel := 
-        | ch (n: nat) .  
-        where n is a De Bruijn index ?
-      *) 
-   
-
-
-      (* let's  say that we have
-         Inductive channel := 
-         | ch (n: nat) .  
-         now to proceed in the proof we do a case analysis on n.
-         [if n=0]: we would have 
-           Hse: (shift e) ⟶[ActOut ((ch 0) ⋉ d)] E
-           so by lts_shift_inv_mu (modulo some details), we have
-               e ⟶[bla] _     /\    (ch 0) ⋉ d = ash bla   
-           ("ash" means "action shift")
-           in particular ch 0 = ch (... +1). impossible.
-        
-         [if n=k+1]: we would have
-            Hp : p ⟶[ActIn (ch (k+1) ⋉ d)] p'
-            Hse: (shift e) ⟶[ActOut ((ch (k+1)) ⋉ d)] E
-           hence
-             Hp : p ⟶[ash (ActIn ((ch k) ⋉ d)) ] p' 
-             Hse: (shift e) ⟶[ ash (ActOut ((ch k) ⋉ d)) ] E
-           now, 
-             with Hp, we get: 
-                 (ν p) ⟶[ActIn ((ch k) ⋉ d)]  (ν p')
-
-             and with  lts_shift_in_mu, we get:
-                 e ⟶[bla] e'   /\ E= shift e' /\ ash (ActOut ((ch k) ⋉ d)) = ash bla.
-             so we also have by ash_inj:
-                  ActOut ((ch k) ⋉ d)=bla
-             hence we get the following fully dual transitions:
-                   e ⟶[ActOut ((ch k) ⋉ d)] e'
-                   (ν p) ⟶[ActIn ((ch k) ⋉ d)]  (ν p')
-             Hence by H1 we get as desired:   
-               p' must_pass shift e'             
-               
-          -------------
-          so basically the idea behind the argument is that:
-          p and e communicate along a channel c and we distinguish two case:  
-            (1)   c is the restricted channel 
-                        or 
-            (2)   c is not the restricted channel.
-         in case (1): 
-            by definition of ((ν c) p)⟶[μ] _ , 
-            the communication between p and e along c is 
-            impossible so we conclude by exfalso.
-         in case (2):
-            p and e communicate along c which is not the restricted channel so
-            everything is ok and we can conclude by induction hypo.
-      *) 
-      admit.  
-Admitted.       
+  * intros p' E ? ? Hpi Hp Hse.  
+    set (lem:= lts_shift_inv_mu _ _ _ Hse).
+    destruct lem as [e' [mue [He [Hsh Hash]] ]]; subst.
+    set (lem:= dual_shift_inv _ _ Hpi). 
+    destruct lem as [mup [Hash Hpipe]]; subst.
+    eapply H1; try apply Hpipe; try constructor; eauto.
+Qed.       
 
 
 
