e2429497e748e35f377709029ce83cf363984710
skanpl <savankan089@gmail.com>
2026-07-01 03:04:57 +0200

    stdpp hell

--- theories/VACCScompose/AltCompose.v
+++ theories/VACCScompose/AltCompose.v
@@ -1,6 +1,6 @@
 
 (*--------------a bunch of imports--------------------*)
-From Stdlib Require Export Program.Equality.
+From Stdlib Require Export Program.Equality .
 From TestingTheory Require Export Must VACCS_Good gLts InteractionBetweenLts ActTau.
 Include VACCS_Testing.
 
@@ -14,6 +14,12 @@ Require Import stdpp.base.
  
 
 Require Import ForwarderConstruction.
+
+
+(*-----temporary ???-----*)
+From stdpp Require Import gmap.
+(*--------------------*)
+(**)
 (*------------------------------------------------------*)
 
 Print FW_gLts.
@@ -33,9 +39,9 @@ Hint Constructors lts :mdb.
 Notation Linp c v :=  ( ActTau.ActExt (InputOutputActions.ActIn (c ⋉ v)) ).
 Notation Lout c v := ( ActTau.ActExt (InputOutputActions.ActOut (c ⋉ v))  ).
 Notation Ltau := ActTau.τ.
-Notation sub t1 x1 := (t1 ^ x1).
+Notation sub t1 x1 :=  (subst_in_proc 0 x1 t1).
 
-
+Locate "^".
 
 
 
@@ -56,14 +62,14 @@ Notation sub t1 x1 := (t1 ^ x1).
 
 Lemma term_tau: forall p:proc, (tau p) ⤓ -> p⤓.
 Proof.
-intros; inversion H.
+intros ? H; inversion H.
 eapply H0; constructor.
 Qed.
 
 Lemma term_tau_rev: forall p:proc, p⤓ -> (tau p)⤓.
 Proof.
-intros; constructor; intros. 
-inversion H0; subst; auto.
+intros; constructor; intros ? Htau. 
+inversion Htau; subst; auto.
 Qed.
 
 Lemma cnv_tau: forall p s,
@@ -85,16 +91,7 @@ inversion Hwt; inversion l; subst.
 eapply H0; auto.
 Qed.
 (*-----------------------------------------*)
-
-
-(*
-Lemma wt_tau: forall q Q s, wt (g (tau q)) s Q -> (~ exists Q', lts Q Ltau Q' ) -> 
-  wt q s Q .
-Proof.
-intros ? ? ? Hwt.
-inversion Hwt; subst.
-intros.
-*)
+ 
 
 Lemma lcnv_comp_tau: forall p q,
   p ≼₁ q ->  (tau p)  ≼₁ (tau q) . 
@@ -168,7 +165,6 @@ Qed.
 
 
 
-
 Lemma lacc_comp_inp: forall x p q,
  (forall v, sub p v ≼₂ sub q v) ->  inp x p  ≼₂ inp x q. 
 Proof.
@@ -177,8 +173,24 @@ unfold "≼₂"; intros ? Q Hcnv Hwt Href.
 destruct s. 
 - exists (inp x p); repeat split; eauto; try constructor.
   inversion Hwt; try inversion l; subst. 
-  (*********) 
-   admit.
+  
+  (**** tentativede preuve 
+         R(x?(y).p)⊆R(x?(y).q )*****)
+   clear  Hwt Href Hcnv Hplq.
+   intro. intro. 
+   destruct H as [mu [G1 G2]]; subst. 
+   exists mu.  
+   split. 
+   + destruct G1 as [mu0 [G1 [G2 G3]]].
+     exists mu0; repeat split; eauto.
+     intro. inversion H.  
+     destruct mu0, (decide (ChannelData_of a = x)).
+     * inversion H1.
+     * admit.
+     * admit.
+     * admit.
+   + unfold 𝝳ᴠᴀᴄᴄꜱ, "∘", Φᴠᴀᴄᴄꜱ.
+     destruct mu,a; auto.      
   (*********)
 - set (lem:= wt_inp _ _ _ _ _ Hwt).
   destruct lem as [v [Heq Hwtsub]].
@@ -202,3 +214,166 @@ Proof.
 unfold "≼ₐₛ"; intros ? ? ? Hplq; split;
 try eapply lcnv_comp_inp; try eapply lacc_comp_inp; apply Hplq.
 Qed.
+
+
+(*==================  Nu  =============================*)
+
+Notation ash mu :=  (VarC_action_add 1 mu).
+
+(*shift on trace*)
+Fixpoint sh s := match s with
+  | nil   => nil
+  | mu::s' => (ash mu):: sh s'  
+end.
+
+
+
+Lemma shift_nil: forall s, nil=sh s -> s=nil.
+Proof. 
+intros ? H; destruct s; auto.
+cbn in H; inversion H.
+Qed.
+
+Lemma shift_cons: forall mu s s', mu::s'=sh s -> 
+  exists mu0 s0, s=mu0::s0 /\ 
+  mu= ash mu0 /\ s'=sh s0.
+Proof.
+intros ? ? ? H; destruct s; cbn in *; 
+inversion H; eauto.
+Qed.
+
+
+Lemma wt_unshift: forall p q s, wt p (sh s) q -> 
+  wt (ν p) s (ν q).  
+Proof.
+intros ? ? ? Hwt.
+dependent induction Hwt.
+- apply shift_nil in x; subst; constructor.
+- specialize (IHHwt s JMeq_refl);  
+  eapply WeakTransitions.wt_tau;  
+  try constructor; eauto. 
+- apply shift_cons in x.
+  destruct x as [mu0[s1[Heqs[Heqmu Heqs0]]]]; subst.
+  specialize (IHHwt _ JMeq_refl).  
+  eapply WeakTransitions.wt_act; 
+  try constructor; eauto.
+Qed.
+
+
+Lemma wt_unshiftmu: forall p q mu, wt p [ash mu] q -> 
+  wt (ν p) [mu] (ν q).  
+Proof.
+eauto using wt_unshift.
+Qed.
+
+
+Lemma wt_shift: forall p Q s, wt (ν p) s Q -> 
+  exists p', wt p (sh s)  p' /\ Q=ν p'.
+Proof.
+intros ? ? ? Hwt.
+dependent induction Hwt; eauto with mdb. 
+- inversion l; subst. 
+  specialize (IHHwt _ eq_refl). 
+  destruct IHHwt as [p''[Hwtp' Heq]]. 
+  eauto with mdb.
+- inversion l; subst.
+  specialize (IHHwt _ eq_refl).
+  destruct IHHwt as [p''[Hwtp' Heq]]; subst. 
+  exists p''; split; 
+  try eapply WeakTransitions.wt_act; eauto.
+Qed.
+
+Lemma wt_shiftmu: forall p Q mu, wt (ν p) [mu] Q -> 
+  exists p', wt p [ash mu]  p' /\ Q=ν p'.
+Proof. 
+intros ? ? ? H.
+eauto using (wt_shift _ _ _ H).
+Qed.
+
+
+
+Lemma term_nu: forall p, (ν p)⤓ -> p⤓.
+Proof.
+intros ? Hter; dependent induction Hter. 
+constructor; intros p' Hp.
+eapply H0; constructor; eauto.
+Qed.
+
+Lemma term_nu_rev: forall p, p⤓ -> (ν p)⤓.
+Proof.
+intros ? Hter; dependent induction Hter.
+constructor; intros P Hnup; inversion Hnup.
+eauto with mdb.
+Qed.
+
+
+
+
+
+
+Lemma cnv_nu: forall s p,
+  (ν p)⇓s -> p⇓(sh s) .
+Proof.
+intros ? ?; generalize dependent p. 
+induction s; intros ? Hcnv; 
+inversion Hcnv; subst; cbn in *; 
+try (constructor; auto using term_nu). 
+intros p' Hwt; inversion Hwt; subst.
+- eapply wt_unshiftmu in w.
+  eapply IHs, H3, wt_unshiftmu; auto.
+- apply lts_res_ext, mu_impl_wt in l.
+  specialize (H3 _ l); specialize (IHs _ H3).
+  eapply cnv_preserved_by_wt_nil; eauto.
+Qed.
+
+Lemma cnv_nu_rev: forall s p,
+  p⇓(sh s) -> (ν p)⇓s.
+Proof.
+intros ? ? Hcnv.
+dependent induction Hcnv.
+- apply shift_nil in x; subst; constructor;
+  auto using term_nu_rev.
+- apply shift_cons in x; subst.
+  destruct x as [mu0 [s1 [Heqs [Heqmu Heqs0]]]]; subst.
+  constructor; auto using term_nu_rev.
+  intros P Hwt; apply wt_shiftmu in Hwt.
+  destruct Hwt as [p' [Hwt Heq]]; subst; eauto.
+Qed.
+
+
+Lemma lcnv_comp_nu: forall p q,
+  p ≼₁ q ->  (ν p)  ≼₁ (ν q) . 
+Proof.
+unfold "≼₁"; intros ? ? Hplq ? Htaup.
+eapply cnv_nu_rev, Hplq, cnv_nu; auto.
+Qed.
+
+
+
+Lemma lacc_comp_nu: forall p q,
+  p ≼₂ q ->  (ν p)  ≼₂ (ν q) . 
+Proof.
+intros ? ? Hplq; unfold "≼₂".
+intros ? Q Hcnv Hwt Href.
+apply wt_shift in Hwt.
+apply cnv_nu in Hcnv.
+unfold "≼₂" in Hplq.
+destruct Hwt as [q'[Hwt Heq]]; subst.
+specialize (Hplq _ _ Hcnv Hwt).
+assert (q' ↛). set_solver.
+specialize (Hplq H).
+destruct Hplq as [p0 [Hwtp [Hrefp0 Hsubset]]]; subst.
+exists (ν p0); repeat split.
+- apply wt_unshift, Hwtp.
+- set_solver.
+- intro. intro. 
+  destruct H0 as [mu [G1 G2]].
+  assert (mu ∈ Subset_Act.coR p0).
+  unfold Subset_Act.coR in *.
+  destruct G1 as [mu0 [K1 [K2 K3]]]. 
+  eexists; repeat split; eauto. 
+  intro. apply K1. 
+  admit.
+  econstructor; split; eauto.
+  admit.
+Admitted.
